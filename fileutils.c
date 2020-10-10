89b54c85a54c7354fa8e7472fd0bee2bf563874d
Jon Palmisciano <jp@jonpalmisc.com>
2020-10-10 16:57:03 -0400

    style: clang-format/LLVM’d fileutils

--- fileutils.c
+++ fileutils.c
@@ -5,49 +5,46 @@
 
 #define BUFFER_SIZE 512
 
-void fbzero(FILE* f, off_t start, size_t len)
-{
-    static unsigned char zb[BUFFER_SIZE] = { 0 };
+void fbzero(FILE *f, off_t start, size_t len) {
+  static unsigned char zb[BUFFER_SIZE] = {0};
 
-    fseeko(f, start, SEEK_SET);
+  fseeko(f, start, SEEK_SET);
 
-    while (len != 0) {
-        size_t size = MIN(len, sizeof(zb));
-        fwrite(zb, size, 1, f);
+  while (len != 0) {
+    size_t size = MIN(len, sizeof(zb));
+    fwrite(zb, size, 1, f);
 
-        len -= size;
-    }
+    len -= size;
+  }
 }
 
-void fmemmove(FILE* f, off_t dest, off_t src, size_t len)
-{
-      // Allocate a transfer buffer.
-    static unsigned char tb[BUFFER_SIZE];
+void fmemmove(FILE *f, off_t dest, off_t src, size_t len) {
+  static unsigned char tb[BUFFER_SIZE];
 
-    while (len != 0) {
-        size_t size = MIN(len, sizeof(tb));
+  while (len != 0) {
+    size_t size = MIN(len, sizeof(tb));
 
-        // Read the source buffer.
-        fseeko(f, src, SEEK_SET);
-        fread(&tb, size, 1, f);
+    // Read the source buffer.
+    fseeko(f, src, SEEK_SET);
+    fread(&tb, size, 1, f);
 
-        // Write the transfer buffer to the destination.
-        fseeko(f, dest, SEEK_SET);
-        fwrite(tb, size, 1, f);
+    // Write the transfer buffer to the destination.
+    fseeko(f, dest, SEEK_SET);
+    fwrite(tb, size, 1, f);
 
-        len -= size;
-        src += size;
-        dest += size;
-    }
+    len -= size;
+    src += size;
+    dest += size;
+  }
 }
 
-size_t fpeek(void* restrict ptr, size_t size, size_t count, FILE* restrict stream)
-{
-    off_t pos = ftello(stream);
-    size_t result = fread(ptr, size, count, stream);
-    fseeko(stream, pos, SEEK_SET);
+size_t fpeek(void *restrict ptr, size_t size, size_t count,
+             FILE *restrict stream) {
+  off_t pos = ftello(stream);
+  size_t result = fread(ptr, size, count, stream);
+  fseeko(stream, pos, SEEK_SET);
 
-    return result;
+  return result;
 }
 
 int file_exists(const char *p) {
--- fileutils.h
+++ fileutils.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void fbzero(FILE*, off_t start, size_t len);
-void fmemmove(FILE*, off_t dest, off_t src, size_t len);
-size_t fpeek(void* restrict ptr, size_t size, size_t count, FILE* restrict);
+void fbzero(FILE *, off_t start, size_t len);
+void fmemmove(FILE *, off_t dest, off_t src, size_t len);
+size_t fpeek(void *restrict ptr, size_t size, size_t count, FILE *restrict);
 
 int file_exists(const char *p);
 
